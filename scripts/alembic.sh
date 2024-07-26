61d2538dd1ce964133a30767f6ca1ff421ca6c98
qiisqwww <qiisqwww@gmail.com>
2024-07-26 17:26:20 +0300

    for revisiom

--- scripts/alembic.sh
+++ scripts/alembic.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
+## Repeat command until port 5431 on address db is not ready.
+until
 alembic revision --autogenerate -m "Initial"
 alembic upgrade head
+do
+echo "Waiting for database connection for 5 seconds..."
+
+## Wait for 5 seconds before check again.
+sleep 5
+done
+echo "Database server ready..."
