267af00aa78729d1adc36fb6bc622ca19ee9753e
qiisqwww <qiisqwww@gmail.com>
2024-07-26 20:00:08 +0300

    fixed bugs with alembic & docker

--- alembic/env.py
+++ alembic/env.py
@@ -6,7 +6,7 @@ from sqlalchemy import pool
 from alembic import context
 
 from src.config import DB_PORT, DB_NAME, DB_USER, DB_PASS, DB_HOST
-from src.database import Base
+from src.auth.models import User
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -29,7 +29,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = [Base.metadata]
+target_metadata = [User.metadata]
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
